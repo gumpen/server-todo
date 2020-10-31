a9b99b533d002ffa6e78ed15cb13cc13706be487
gumpen <shuhei1993727@gmail.com>
2020-10-31 13:57:34 +0900

    add cors middleware

--- db/migrations/20201031111644-add_column.sql
+++
@@ -1,8 +0,0 @@
-
--- +migrate Up
-ALTER TABLE messages
-ADD updated_at DATETIME default current_timestamp;
-
--- +migrate Down
-ALTER TABLE messages
-DROP updated_at;
--- src/handler/router.go
+++ src/handler/router.go
@@ -5,8 +5,12 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func InitRouting(echo *echo.Echo, messageHandler MessageHandler) {
-	echo.Use(middleware.Logger())
-	echo.GET("/messages", messageHandler.Index())
-	echo.POST("/messages", messageHandler.Create())
+func InitRouting(e *echo.Echo, messageHandler MessageHandler) {
+	e.Use(middleware.Logger())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
+	e.GET("/messages", messageHandler.Index())
+	e.POST("/messages", messageHandler.Create())
 }
